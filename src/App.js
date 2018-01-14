1a1babf28e35ace2f6f069a2e7285078e6a77e7a
Daniel Yim <danielyim@gmail.com>
2018-01-14 15:24:15 -0800

    adding SCSS and css reset; basic style scaffold

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import logo from './assets/images/logo.png'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
   render() {
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Cookie Jar</h1>
+          <h1 className="App-title">Dan's Cookie Jar</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
