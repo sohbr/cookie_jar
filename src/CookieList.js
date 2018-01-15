44327a957fd8bf599c53d5c3669b0e8f7b8fca42
Daniel Yim <danielyim@gmail.com>
2018-01-14 18:00:39 -0800

    handling webp in webpack as a normal unhashed file asset; fixing images and styling for the cookie list

--- src/CookieList.js
+++ src/CookieList.js
@@ -1,43 +1,49 @@
 import React, { Component } from 'react'
 import CookieListItem from './CookieListItem'
 import './CookieList.scss'
+import './assets/images/almondbutter.webp'
+import './assets/images/carrot.webp'
+import './assets/images/chocolatechip.webp'
+import './assets/images/oatmealraisin.webp'
+import './assets/images/peanutbutter.webp'
+import './assets/images/snickerdoodle.webp'
 
 const cookies = [
   {
     id: 'almondbutter',
     title: 'Almond Butter',
     description: 'My almond butter cookie',
-    img_url: './assets/images/almondbutter.webp'
+    img_url: './static/media/almondbutter.webp'
   },
   {
     id: 'carrot',
     title: 'Carrot Cake',
     description: 'My carrot cake cookie',
-    img_url: './assets/images/carrot.webp'
+    img_url: './static/media/carrot.webp'
   },
   {
     id: 'chocolatechip',
     title: 'Chocolate Chip',
     description: 'My chocolate chip cookie',
-    img_url: './assets/images/chocolatechip.webp'
+    img_url: './static/media/chocolatechip.webp'
   },
   {
     id: 'oatmealraisin',
     title: 'Oatmeal Raisin',
     description: 'My OR cookie',
-    img_url: './assets/images/oatmealraisin.webp'
+    img_url: './static/media/oatmealraisin.webp'
   },
   {
     id: 'peanutbutter',
     title: 'Peanut Butter',
     description: 'My PB cookie',
-    img_url: './assets/images/peanutbutter.webp'
+    img_url: './static/media/peanutbutter.webp'
   },
   {
     id: 'snickerdoodle',
     title: 'Snickerdoodle',
     description: 'My snickerdoodle cookie',
-    img_url: './assets/images/snickerdoodle.webp'
+    img_url: './static/media/snickerdoodle.webp'
   }
 ]
 
--- src/CookieListItem.js
+++ src/CookieListItem.js
@@ -11,13 +11,17 @@ class CookieListItem extends Component {
   render() {
     return (
       <div className="cookie-item">
-        <img src={this.props.image} alt={this.props.title} />
-        <h2>
-          {this.props.title}
-        </h2>
-        <p>
-          {this.props.description}
-        </p>
+        <div className="cookie-image">
+          <img src={this.props.image} alt={this.props.title} />
+        </div>
+        <div className="cookie-content">
+          <h2>
+            {this.props.title}
+          </h2>
+          <p>
+            {this.props.description}
+          </p>
+        </div>
       </div>
     )
   }
