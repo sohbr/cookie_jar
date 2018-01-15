6cfbd462e390e545d642fb1ecb7f0e7a38e50bcc
Daniel Yim <danielyim@gmail.com>
2018-01-14 17:37:02 -0800

    adding more cookies

--- src/CookieList.js
+++ src/CookieList.js
@@ -4,24 +4,40 @@ import './CookieList.scss'
 
 const cookies = [
   {
+    id: 'almondbutter',
     title: 'Almond Butter',
     description: 'My almond butter cookie',
     img_url: './assets/images/almondbutter.webp'
   },
   {
+    id: 'carrot',
     title: 'Carrot Cake',
     description: 'My carrot cake cookie',
     img_url: './assets/images/carrot.webp'
   },
   {
+    id: 'chocolatechip',
     title: 'Chocolate Chip',
     description: 'My chocolate chip cookie',
     img_url: './assets/images/chocolatechip.webp'
   },
   {
+    id: 'oatmealraisin',
     title: 'Oatmeal Raisin',
     description: 'My OR cookie',
     img_url: './assets/images/oatmealraisin.webp'
+  },
+  {
+    id: 'peanutbutter',
+    title: 'Peanut Butter',
+    description: 'My PB cookie',
+    img_url: './assets/images/peanutbutter.webp'
+  },
+  {
+    id: 'snickerdoodle',
+    title: 'Snickerdoodle',
+    description: 'My snickerdoodle cookie',
+    img_url: './assets/images/snickerdoodle.webp'
   }
 ]
 
@@ -31,6 +47,7 @@ class CookieList extends Component {
       <div className="cookie-list">
         {cookies.map(c =>
           <CookieListItem
+            key={c.id}
             title={c.title}
             description={c.description}
             image={c.img_url}
