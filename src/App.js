7663b5cf6ea5d1b326fa7ea276d20bda0aa577ea
sohbr <sohnbrian@gmail.com>
2018-01-14 18:51:57 -0800

    remove extra buttons from jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import Contact from './Contact'
-import Button from './Button'
-import CookieList from './CookieList'
-import Footer from './Footer'
-import logo from './assets/images/logo.png'
-import './App.scss'
+import React, { Component } from "react";
+import Button from "./Button";
+import CookieList from "./CookieList";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import logo from "./assets/images/logo.png";
+import "./App.scss";
 
 class App extends Component {
   render() {
@@ -20,16 +20,14 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
           </div>
           <div className="header-right">
-            <h1 className="App-title">Dan's Cookie Jar</h1>
+            <h1 className="App-title">Dan&apos;s Cookie Jar</h1>
             <h2 className="App-intro">
-              Welcome to my cookie jar. I don't have much of a story other than
-              being a hungry guy with an appetite for delicious cookies. And
-              besides, anyone can bake a cookie, we craft an experience. I'll
-              add some cheesy story later...
+              Welcome to my cookie jar. I don&apos;t have much of a story other
+              than being a hungry guy with an appetite for delicious cookies.
+              And besides, anyone can bake a cookie, we craft an experience.
+              I&apos;ll add some cheesy story later...
             </h2>
-            <button className="App-button">Order Now</button>
             <Button width="14vw">Order Now</Button>
-            <Button>Button without a specific height/width</Button>
           </div>
         </header>
         <section className="gallery">
@@ -44,8 +42,8 @@ class App extends Component {
         </section>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
