e66d9de1f793df3c41fe9733e64758e4826576a0
sohbr <sohnbrian@gmail.com>
2018-01-14 17:29:26 -0800

    update header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import logo from './assets/images/logo.png'
-import './App.scss'
+import React, { Component } from "react";
+import logo from "./assets/images/logo.png";
+import "./App.scss";
 
 class App extends Component {
   render() {
@@ -12,8 +12,19 @@ class App extends Component {
           <a href="">Contact</a>
         </navbar>
         <header>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Dan's Cookie Jar</h1>
+          <div className="header-left">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="header-right">
+            <h1 className="App-title">Dan's Cookie Jar</h1>
+            <h2 className="App-intro">
+              Welcome to my cookie jar. I don't have much of a story other than
+              being a hungry guy with an appetite for delicious cookies. And
+              besides, anyone can bake a cookie, we craft an experience. I'll
+              add some cheesy story later...
+            </h2>
+            <button className="App-button">Order Now</button>
+          </div>
         </header>
         <section className="gallery">
           <h2>Gallery</h2>
@@ -26,8 +37,8 @@ class App extends Component {
         </section>
         <footer>© 2017 by DAN'S COOKIE JAR.</footer>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
