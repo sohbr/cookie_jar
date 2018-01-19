ecc8349edc5c21ed15eb46a7e0254290b3e022bf
sohbr <sohnbrian@gmail.com>
2018-01-18 16:37:34 -0800

    adding email link

--- src/Contact.js
+++ src/Contact.js
@@ -14,17 +14,25 @@ class Contact extends Component {
         </div>
         <div className="contact-order">or</div>
         <div className="contact-order">
-          Send us an Email: danscookiejar@gmail.com
+          Send us an Email:
+          <a href="mailto:danscookiejar@gmail.com?Subject=Cookie%20Order">
+            danscookiejar@gmail.com
+          </a>
         </div>
+        <a
+          href="mailto:someone@example.com?Subject=Hello%20again"
+          target="_top"
+        >
+          Send Mail
+        </a>
         <div className="contact-blurb">
           Our website is designed for Houston residents to purchase cookies
           online for pickup. Delivery arrangements can be made for an additional
           charge. We also accept credit cards or cash on delivery or pickup.
-        </div>
-        <div className="contact-blurb">
-          Currently a 2 day advance notice is required. We happily take custom
-          orders for parties, weddings, and special occasions. Just give us a
-          call, text or email and we will be happy to fulfill your order.
+          Currently, a 2 day advance notice is required for all orders. We
+          happily take custom orders for parties, weddings, and special
+          occasions. Just give us a call, text or email and we will be happy to
+          fulfill your order.
         </div>
       </div>
     )
