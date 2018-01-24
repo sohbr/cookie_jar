4df6552ad15c840f5b0385d43acdcda5442c35ae
sohbr <sohnbrian@gmail.com>
2018-01-24 01:21:10 -0800

    added remaining cookies

--- src/CookieList.js
+++ src/CookieList.js
@@ -7,8 +7,20 @@ import './assets/images/chocolatechip.webp'
 import './assets/images/oatmealraisin.webp'
 import './assets/images/peanutbutter.webp'
 import './assets/images/snickerdoodle.webp'
+import './assets/images/pecansandie.webp'
+import './assets/images/sugar.webp'
+import './assets/images/oreo.webp'
+import './assets/images/doublechoco.webp'
+import './assets/images/assorted.webp'
+import './assets/images/white.webp'
 
 const cookies = [
+  {
+    id: 'assorted',
+    title: 'Assorted',
+    description: 'My assorted cookies',
+    img_url: './static/media/assorted.webp'
+  },
   {
     id: 'almondbutter',
     title: 'Almond Butter',
@@ -28,10 +40,28 @@ const cookies = [
     img_url: './static/media/chocolatechip.webp'
   },
   {
+    id: 'chocolatechipwalnut',
+    title: 'Chocolate Chip Walnut',
+    description: 'My chocolate chip walnut cookie',
+    img_url: './static/media/chocolatechip.webp'
+  },
+  {
+    id: 'doublechocolatechipwalnut',
+    title: 'Double Chocolate Chip Walnut',
+    description: 'My double chocolate chip walnut cookie',
+    img_url: './static/media/doublechoco.webp'
+  },
+  {
     id: 'oatmealraisin',
     title: 'Oatmeal Raisin',
     description: 'My OR cookie',
     img_url: './static/media/oatmealraisin.webp'
+  },
+  {
+    id: 'oreo',
+    title: 'Oreo',
+    description: 'My oreo cookie',
+    img_url: './static/media/oreo.webp'
   },
   {
     id: 'peanutbutter',
@@ -40,10 +70,28 @@ const cookies = [
     img_url: './static/media/peanutbutter.webp'
   },
   {
+    id: 'pecansandie',
+    title: 'Pecan Sandie',
+    description: 'My pecan sandie cookie',
+    img_url: './static/media/pecansandie.webp'
+  },
+  {
     id: 'snickerdoodle',
     title: 'Snickerdoodle',
     description: 'My snickerdoodle cookie',
     img_url: './static/media/snickerdoodle.webp'
+  },
+  {
+    id: 'sugar',
+    title: 'Sugar',
+    description: 'My sugar cookie',
+    img_url: './static/media/sugar.webp'
+  },
+  {
+    id: 'whitechocolatewalnut',
+    title: 'White Chocolate Walnut',
+    description: 'My white chocolate walnut cookie',
+    img_url: './static/media/white.webp'
   }
 ]
 
