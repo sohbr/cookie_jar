bc2d5235f3b74c09e40032ad0d0a4e81189261ac
Daniel Yim <danielyim@gmail.com>
2018-01-31 00:52:09 -0800

    removing and adding the right dep; styling modals and doing initial work for displaying detailed cookie info

--- src/CookieListItem.js
+++ src/CookieListItem.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react'
+import Modal from 'react-modal'
 import PropTypes from 'prop-types'
 
+const customModalStyle = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxHeight: '75%',
+    maxWidth: '75%'
+  }
+}
+
+Modal.setAppElement('#root')
+
 class CookieListItem extends Component {
+  state = { isOpen: false }
+
   static propTypes = {
     title: PropTypes.string.isRequired,
+    headline: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired
   }
 
+  handleClickCard = () => {
+    this.setState({ isOpen: true })
+  }
+
   render() {
     return (
-      <li className="cookie-item">
+      <li className="cookie-item" onClick={this.handleClickCard}>
         <div className="cookie-image">
           <img src={this.props.image} alt={this.props.title} />
         </div>
@@ -19,9 +42,29 @@ class CookieListItem extends Component {
             {this.props.title}
           </h2>
           <p>
-            {this.props.description}
+            {this.props.headline}
           </p>
         </div>
+        <Modal
+          isOpen={this.state.isOpen}
+          onRequestClose={() => this.setState({ isOpen: false })}
+          contentLabel={this.props.title}
+          style={customModalStyle}
+        >
+          <div className="cookie-modal">
+            <div className="cookie-modal--image">
+              <img src={this.props.image} alt={this.props.title} />
+            </div>
+            <div className="cookie-modal--content">
+              <h1>
+                {this.props.title}
+              </h1>
+              <p>
+                {this.props.description}
+              </p>
+            </div>
+          </div>
+        </Modal>
       </li>
     )
   }
