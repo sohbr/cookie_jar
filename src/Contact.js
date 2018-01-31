32c1d8189117fea33c1f6f73d3354c6499ea1249
Daniel Yim <danielyim@gmail.com>
2018-01-31 00:03:43 -0800

    creating a parameterized styled component to contain the map element

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import './Contact.scss'
 import MapContainer from './MapContainer'
 import cupcake1 from './assets/images/cupcake1.png'
@@ -7,13 +8,21 @@ import fb from './assets/images/fb-logo.png'
 import ig from './assets/images/ig-logo.png'
 import tw from './assets/images/tw-logo.png'
 
+const SizeableDiv = styled.div`
+  margin-top: 25px;
+  width: ${props => (props.width ? props.width : '400px')};
+  height: ${props => (props.height ? props.height : '400px')};
+`
 class Contact extends Component {
+  mapWidth = '700px'
+  mapHeight = '700px'
+
   render() {
     return (
       <div className="contact">
-        <div className="map">
-          <MapContainer />
-        </div>
+        <SizeableDiv width={this.mapWidth} height={this.mapHeight}>
+          <MapContainer width={this.mapWidth} height={this.mapHeight} />
+        </SizeableDiv>
         <div className="contact-order">
           Order by Phone or Text: 832-830-5599
         </div>
@@ -28,7 +37,13 @@ class Contact extends Component {
           occasions. Just give us a call, text or email and we will be happy to
           fulfill your order.
         </div>
-        <a href="mailto:danscookiejar@gmail.com">danscookiejar@gmail.com</a>
+        <a
+          href="mailto:danscookiejar@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          danscookiejar@gmail.com
+        </a>
         <img className="cupcake" src={cupcake1} alt={'cupake1'} />
         <div className="follow-us">
           <img className="star" src={star} alt={'star'} />
