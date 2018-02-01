e5b4d56bf5f0c40b648c3f68b1ad71e126c87c57
sohbr <sohnbrian@gmail.com>
2018-02-01 14:53:00 -0800

    added carousel styling

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Slider from 'react-slick'
+import styled from 'styled-components'
 import './Carousel.scss'
 
 import c1 from './assets/images/c1.png'
@@ -10,7 +11,16 @@ import c5 from './assets/images/c5.png'
 import c6 from './assets/images/c6.png'
 import c7 from './assets/images/c7.png'
 
+const CarouselDiv = styled.div`
+  margin-top: 25x;
+  width: ${props => (props.width ? props.width : '400px')};
+  height: ${props => (props.height ? props.height : '400px')};
+`
+
 class Carousel extends Component {
+  carouselWidth = '500px'
+  carouselHeight = '500px'
+
   render() {
     const settings = {
       dots: true,
@@ -24,29 +34,31 @@ class Carousel extends Component {
     }
     return (
       <div className="carousel">
-        <Slider className="slider" {...settings}>
-          <div>
-            <img width={'200px'} height={'auto'} src={c1} alt="c1" />
-          </div>
-          <div>
-            <img width={'200px'} height={'auto'} src={c2} alt="c2" />
-          </div>
-          <div>
-            <img width={'200px'} height={'auto'} src={c3} alt="c3" />
-          </div>
-          <div>
-            <img width={'200px'} height={'auto'} src={c4} alt="c4" />
-          </div>
-          <div>
-            <img width={'200px'} height={'auto'} src={c5} alt="c5" />
-          </div>
-          <div>
-            <img width={'200px'} height={'auto'} src={c6} alt="c6" />
-          </div>
-          <div>
-            <img width={'200px'} height={'auto'} src={c7} alt="c7" />
-          </div>
-        </Slider>
+        <CarouselDiv width={this.carouselWidth} height={this.carouselHeight}>
+          <Slider className="slider" {...settings}>
+            <div>
+              <img className="cookie-pics" src={c1} alt="c1" />
+            </div>
+            <div>
+              <img className="cookie-pics" src={c2} alt="c2" />
+            </div>
+            <div>
+              <img className="cookie-pics" src={c3} alt="c3" />
+            </div>
+            <div>
+              <img className="cookie-pics" src={c4} alt="c4" />
+            </div>
+            <div>
+              <img className="cookie-pics" src={c5} alt="c5" />
+            </div>
+            <div>
+              <img className="cookie-pics" src={c6} alt="c6" />
+            </div>
+            <div>
+              <img className="cookie-pics" src={c7} alt="c7" />
+            </div>
+          </Slider>
+        </CarouselDiv>
       </div>
     )
   }
