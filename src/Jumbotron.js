ca31be66ea4cd2b3bc4dbde5c61390460e779896
sohbr <sohnbrian@gmail.com>
2018-05-07 20:02:01 -0700

    finished jumbotron styling

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from './Button'
 import logo from './assets/images/logo.png'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 import './Jumbotron.scss'
 
 const Jumbotron = props =>
@@ -11,16 +12,24 @@ const Jumbotron = props =>
       <img src={logo} className="logo" alt="logo" />
 
       <h1>DAN'S COOKIE JAR</h1>
-      <h2>
-        Welcome to my cookie jar. I don&apos;t have much of a story other than
-        being a hungry guy with an appetite for delicious cookies. And besides,
-        anyone can bake a cookie, we craft an experience. I&apos;ll add some
-        cheesy story later...
-      </h2>
-      <Button width="150px">Order Now</Button>
+      <div className="star-divider">
+        <div className="bar"></div>
+        <i className="fa fa-star fa-3x" aria-hidden="true"></i>
+        <div className="bar"></div>
+      </div>
+
+      <Button width="150px">ORDER NOW</Button>
     </div>
     <div className="header-right">
     </div>
   </header>
 
 export default Jumbotron
+
+
+// <h2>
+//   Welcome to my cookie jar. I don&apos;t have much of a story other than
+//   being a hungry guy with an appetite for delicious cookies. And besides,
+//   anyone can bake a cookie, we craft an experience. I&apos;ll add some
+//   cheesy story later...
+// </h2>
